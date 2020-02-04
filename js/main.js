83f1a4823f60edd49df214255b8cd7a018cb495a
Gino Ghalehpardaz <ginoghalehpardaz@Ginos-MBP-2.fc.ca>
2020-02-04 16:50:07 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+let SplashComponent = {
+    template: `<h1>Welcome to my Super Awesome App!</h1>`
+}
+
+let AppComponent = {
+    template: `<h1>This is the app page</h1>`
+}
+
+let ErrorComponent = {
+    template: `<h1>That page doesnt exist</h1>`
+}
+
+
+//these are the same as Express routes -> router.get('/', ....do something with the request)
+const routes = [
+    {path: '/', name: 'splash', component: SplashComponent },
+    {path: '/app', name: 'app', component: AppComponent },
+    {path: '*', name: 'error', component: ErrorComponent} 
+    // the last one always has to be the one with the path '•' one
+]
+
+const router = new VueRouter({
+    routes // short for routes: routes
+})
+
+
+
+const vm = new Vue({
+    data: {
+
+
+    },
+
+    methods: {
+
+
+    },
+
+    router
+}).$mount("#app");
